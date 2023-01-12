4b019adc3b73944e314c2b285453ea246a6379b0
silvanoamaral <silvano.amaral9@gmail.com>
2023-01-12 20:41:37 -0300

    ✨ config redux-saga

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+
 import counterReducer from "../features/counter/counterSlice";
+import userReducer from "../reduxToolkit/reducers";
+import rootSaga from "../reduxToolkit/sagas";
+
+let sagaMiddleware = createSagaMiddleware();
 
 export const reducer = {
   counter: counterReducer,
+  users: userReducer,
 };
+
+const middlewares = (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(sagaMiddleware);
 
 export const store = configureStore({
   reducer,
+  middleware: middlewares,
+  devTools: process.env.NODE_ENV !== "production",
 });
+
+sagaMiddleware.run(rootSaga);
+
+// export const setupStore = (preloadedState) => {
+//   return configureStore({
+//     reducer,
+//     preloadedState,
+//     middlewares,
+//   });
+// };
